00bfe3312f6ef96069d80067c294715214d42418
pambaka <pambaka@ya.ru>
2024-03-28 16:03:18 +0100

    feat: getPromise is implemented

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,16 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject(number);
+    } else {
+      resolve(number);
+    }
+  });
+
+  return promise;
 }
 
 /**
